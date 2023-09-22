d8fb084199c34a86769ef6b7a85ff3d4340a2f68
shadarf779 <sa1121@cs.soran.edu.iq>
2023-09-22 21:08:09 +0300

    update all links

--- include/links.php
+++ include/links.php
@@ -1,1 +1,34 @@
 
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+  rel="stylesheet">
+  <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
+  rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/vendors.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/app.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/menu/menu-types/vertical-menu-modern.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/core/colors/palette-gradient.css">
+  <link rel="stylesheet" type="text/css" href="app-assets/css/plugins/forms/extended/form-extended.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<script src="app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+
+<script src="app-assets/vendors/js/forms/extended/typeahead/typeahead.bundle.min.js"
+type="text/javascript"></script>
+<script src="app-assets/vendors/js/forms/extended/typeahead/bloodhound.min.js"
+type="text/javascript"></script>
+<script src="app-assets/vendors/js/forms/extended/typeahead/handlebars.js"
+type="text/javascript"></script>
+<script src="app-assets/vendors/js/forms/extended/inputmask/jquery.inputmask.bundle.min.js"
+type="text/javascript"></script>
+<script src="app-assets/vendors/js/forms/extended/formatter/formatter.min.js"
+type="text/javascript"></script>
+<script src="../../../app-assets/vendors/js/forms/extended/maxlength/bootstrap-maxlength.js"
+type="text/javascript"></script>
+<script src="app-assets/vendors/js/forms/extended/card/jquery.card.js" type="text/javascript"></script>
+<script src="app-assets/js/core/app-menu.js" type="text/javascript"></script>
+<script src="app-assets/js/core/app.js" type="text/javascript"></script>
+<script src="app-assets/js/scripts/customizer.js" type="text/javascript"></script>
+<script src="app-assets/js/scripts/forms/extended/form-typeahead.js" type="text/javascript"></script>
+<script src="app-assets/js/scripts/forms/extended/form-inputmask.js" type="text/javascript"></script>
+<script src="app-assets/js/scripts/forms/extended/form-formatter.js" type="text/javascript"></script>
+<script src="app-assets/js/scripts/forms/extended/form-maxlength.js" type="text/javascript"></script>
+<script src="app-assets/js/scripts/forms/extended/form-card.js" type="text/javascript"></script>
